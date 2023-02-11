d4a0083fb0424982f2f773519f117ab1e34f3a00
Maria <m.tur67@gmail.com>
2023-02-11 07:50:41 +0300

    add homework3

---
+++ homework_3/script.js
@@ -0,0 +1,13 @@
+
+let name = prompt('Введите Ваше имя?', '')
+alert(`${name}`);
+let userName = `${name}`
+function showMessage() {
+    let message = `Здравствуйте, ${userName}!`;
+    alert(message);
+}
+showMessage();
+
+    
+
+
